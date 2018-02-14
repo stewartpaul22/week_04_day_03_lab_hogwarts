f3bebc6ce773596f3b30f462ae64e8f5a4e41515
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-14 20:59:59 +0000

    Change @house to @house_id throughout the Student class

--- models/student.rb
+++ models/student.rb
@@ -2,20 +2,20 @@ require_relative('../db/sql_runner')
 
 class Student
 
-  attr_accessor :id, :first_name, :second_name, :house, :age
+  attr_accessor :id, :first_name, :second_name, :house_id, :age
 
   def initialize( options )
     @id = options['id'].to_i
     @first_name = options['first_name']
     @second_name = options['second_name']
-    @house = options['house']
+    @house_id = options['house_id']
     @age = options['age'].to_i
   end
 
 
   def save()
-    sql = "INSERT INTO students (first_name, second_name, house, age) VALUES ($1, $2, $3, $4) RETURNING id"
-    values = [@first_name, @second_name, @house, @age]
+    sql = "INSERT INTO students (first_name, second_name, house_id, age) VALUES ($1, $2, $3, $4) RETURNING id"
+    values = [@first_name, @second_name, @house_id, @age]
     student = SqlRunner.run( sql, values )
     @id = student.first()['id'].to_i
   end
