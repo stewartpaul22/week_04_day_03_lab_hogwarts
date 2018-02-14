7c3c351caf7507d51745167aece8dd880d60f250
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-14 20:55:22 +0000

    Change house VARCHAR to house_id in hogwarts.sql

--- db/hogwarts.sql
+++ db/hogwarts.sql
@@ -1,15 +1,15 @@
-DROP TABLE houses;
 DROP TABLE students;
+DROP TABLE houses;
+
+CREATE TABLE houses (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
 
 CREATE TABLE students (
   id SERIAL4 PRIMARY KEY,
   first_name VARCHAR(255),
   second_name VARCHAR(255),
-  house VARCHAR(255),
+  house_id INT REFERENCES houses(id) ON DELETE CASCADE,
   age INT
-);
-
-CREATE TABLE houses (
-  id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255)
 );
