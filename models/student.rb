2bbc04c9ce8fb293239b852ed1e41713b453e0f7
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-02-14 13:16:19 +0000

    Add initialize method to Student class and created sql_runner.rb

--- models/student.rb
+++ models/student.rb
@@ -2,7 +2,19 @@ require_relative('../db/sql_runner')
 
 class Student
 
-  
+  attr_accessor :id, :first_name, :second_name, :house, :age
 
+  def initialize( options )
+    @id = options['id'].to_i
+    @first_name = options['first_name']
+    @second_name = options['second_name']
+    @house = options['house']
+    @age = options['age'].to_i
+  end
+
+
+  def save()
+    
+  end
 
 end
